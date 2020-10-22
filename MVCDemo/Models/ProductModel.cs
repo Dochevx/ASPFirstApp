f668b6ee60c4c9a5b1bd6078dd5384ae3beba523
Georgi Dochev <georgi.p.dochev@gmail.com>
2020-10-22 17:04:18 +0300

    Uploading the project

---
+++ MVCDemo/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
